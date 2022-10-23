65d56094538e0ff6002aacb44c000c1865d6ba0c
Jamal Khan <jamal919@gmail.com>
2022-10-23 16:42:51 +0200

    :memo: Fix cycle selection bug

--- src/App.js
+++ src/App.js
@@ -98,7 +98,7 @@ export default function App() {
           <BasicNavbar
             sitename={config.sitename}
             navbar={config.navbar}
-            forecast={platformStatus.data.lastforecast}
+            forecast={selectedforecast}
             setForecast={setSelectedForecast}
             cycles={config.cycles}
           ></BasicNavbar>
