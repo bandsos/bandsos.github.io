2a44ee79c78812e9fb50ec837d34ba47252f88a8
rkmaxhero <rounaqkhan12@gmail.com>
2024-07-16 11:59:04 -0400

    Made changes as request by Jamal Bhai

--- src/components/TimeSlider.js
+++ src/components/TimeSlider.js
@@ -60,24 +60,36 @@ const TimeSlider = function ({forecast, setTimestep}) {
                 
                 <div class="buttonContainer">
                     <div>
-                        <button class="sliderButton" onClick={() => handleStepChange('left')}>&lt;</button>
+                        <Button type="button" class="sliderButton btn btn-primary btn-sm" onClick={() => handleStepChange('left')}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+                                <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
+                            </svg>
+                        </Button>
                     </div>
-                        
+
                     <div>
-                        <button class="sliderButton" onClick={() => handleStepChange('right')}>&gt;</button>
+                        <Button type="button" class="sliderButton btn btn-primary btn-sm" onClick={() => handleStepChange('right')}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+                            </svg>
+                        </Button>
                     </div>
                 </div>
 
                 <div class="mobileDisplay">
 
-                    <div class = "mobileButtons">
-                        <div>
-                            <button class="sliderButton" onClick={() => handleStepChange('left')}>&lt;</button>
-                        </div>
-                            
-                        <div>
-                            <button class="sliderButton" onClick={() => handleStepChange('right')}>&gt;</button>
-                        </div>
+                    <div>
+                        <Button type="button" class="sliderButton btn btn-primary btn-sm" onClick={() => handleStepChange('left')}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+                                <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
+                            </svg>
+                        </Button>
+                        
+                        <Button type="button" class="sliderButton btn btn-primary btn-sm" onClick={() => handleStepChange('right')}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+                            </svg>
+                        </Button>
                     </div>
 
                     <div id="buble"><b>{steps[index].time}</b></div>
