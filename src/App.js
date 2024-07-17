d475f7783a26e4fb4b7a2b562036779074eb7587
Rounaq Khan <45477562+rkmaxhero@users.noreply.github.com>
2024-07-17 04:02:57 -0400

    Update Timeslider (#1)
    * Got "overlay" effect with time slider
    * Time slider mobile version.
    * Fixed spacing issues

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import Map from "./components/Map";
 import BasicNavbar from "./components/Navbar";
 import TimeSlider from "./components/TimeSlider";
 
+import NewNavbar from "./molecules/rkNavbar";
+import NewTimeSlider from "./molecules/rkTimeSlider";
+
 import config from "./config";
 
 // Stylesheets
 import "leaflet/dist/leaflet.css";
 import "./App.css";
 import { Alert, Container } from "react-bootstrap";
+
+//run command: npm start
+
+/*
+  Rounaq WTF comments:
+  - Layer Control is pretty tricky. But they don't go to the highest possible numerical z-index: 2147483647
+  - what is <output> tag in TimeSlider.js? 
+  
+*/
 
 // The main application
 export default function App() {
@@ -115,7 +127,9 @@ export default function App() {
             forecast={selectedforecast}
             setForecast={setSelectedForecast}
             cycles={config.cycles}
-          ></BasicNavbar>
+          >
+          </BasicNavbar>
+          {/* <NewNavbar></NewNavbar> */}
           </div>
         : <div id="header">
           <BasicNavbar
@@ -132,14 +146,19 @@ export default function App() {
         forecast.available
         ? <>
           <div id="content">
+          
           <Map
             dataurl={selectedforecast.dataurl}
             config={config.map}
             forecast={forecast.payload}
             timestep={timestep}
-          ></Map>
+            setTimestep={setTimestep}
+          >
+          </Map>
           </div>
-          <TimeSlider forecast={forecast.payload} setTimestep={setTimestep}></TimeSlider>
+          <div id="onTop">
+              <TimeSlider forecast={forecast.payload} setTimestep={setTimestep}></TimeSlider>
+          </div>
           </>
         : <Container>
           <Alert className="" variant="danger" key="danger">
@@ -150,3 +169,6 @@ export default function App() {
     </div>
   );
 }
+
+// const styles = StyleSheet.create({
+// });
